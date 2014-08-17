65f67c9a21bd2443d47b06679432ae0848b09649
takuhito-h <bluesbug@gmail.com>
2014-08-17 22:44:50 +0900

    ログに色付けをした

--- tasks/check_img_damage.js
+++ tasks/check_img_damage.js
@@ -6,6 +6,10 @@ module.exports = function (grunt) {
         var done = this.async();
         var options = this.options();
 
+        var green_code = "\u001b[32m";
+        var red_code = "\u001b[31m";
+        var reset_code = "\u001b[0m";
+
         grunt.util.spawn({
             cmd: "ruby",
             args: ["./node_modules/grunt-check-img-damage/check_img_damage.rb", options.globPath]
@@ -14,20 +18,20 @@ module.exports = function (grunt) {
             // 終了コードをチェック。ログを出力。
             switch(code){
                 case 0:
-                    grunt.log.ok("success");
+                    grunt.log.ok(green_code + "success!" + reset_code);
                     break;
                 case 1:
                     grunt.log.writeln(result.stdout);
-                    grunt.log.errorlns("caution!! ファイルが破損しています。");
+                    grunt.log.errorlns(red_code + "caution!! ファイルが破損しています。" + reset_code);
                     break;
                 case 2:
                     grunt.log.writeln(result.stdout);
-                    grunt.log.errorlns("caution!! 拡張子が間違っています。");
+                    grunt.log.errorlns(red_code + "caution!! 拡張子が間違っています。" + reset_code);
                     break;
                 case 3:
                     grunt.log.writeln(result.stdout);
-                    grunt.log.errorlns("caution!! ファイルが破損しています。");
-                    grunt.log.errorlns("caution!! 拡張子が間違っています。");
+                    grunt.log.errorlns(red_code + "caution!! ファイルが破損しています。" + reset_code);
+                    grunt.log.errorlns(red_code + "caution!! 拡張子が間違っています。" + reset_code);
                     break;
             }
 
